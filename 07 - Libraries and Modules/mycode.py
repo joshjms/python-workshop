df070483203b74c8ca745cfd5f896d14537f3c31
JJAMES001@e.ntu.edu.sg <83194022+joshjms@users.noreply.github.com>
2023-08-25 17:49:49 +0800

    libraries and modules

---
+++ 07 - Libraries and Modules/mycode.py
@@ -0,0 +1,5 @@
+from mymodule import very_complex_code, another_very_complex_code, yet_another_very_complex_code
+
+print(very_complex_code(1))
+print(another_very_complex_code(1))
+print(yet_another_very_complex_code(1))
---
+++ 07 - Libraries and Modules/mymodule.py
@@ -0,0 +1,8 @@
+def very_complex_code(n):
+    return n + 1
+
+def another_very_complex_code(n):
+    return n + 2
+
+def yet_another_very_complex_code(n):
+    return n + 3
